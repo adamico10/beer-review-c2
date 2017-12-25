4370362bd63348ea6122be2ab3be17862fed5832
Adam Shmeiss <34445682+adamico10@users.noreply.github.com>
2017-12-25 14:56:26 +0200

    sort by rating

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
+// creating an array to get the inputs
 var coffee = [];
-
+// function invoked when buttom clicked
 function addCoffee(name, category, rating) {
     coffee.push({"coffeeName": name, "categoryC": category, "userRating": rating})
 }
-
+// what to start when there is a click
 $('.post-coffee').click(function() {
-    addCoffee($('.coffee-input').val(),$('.category-input').val(),$('.rating-input').val())
+    addCoffee($('.coffee-input').val(),$('.category-input').val(),$('.selector-input').val())
 }
 )
 
+// clears the current list
 function clearList() {
     $('.coffee-list').find('li').remove();
 }
-    
+ 
+// rendering the results
 $('.render-coffee').click(function() {
     clearList();   
     for(var i = 0; i < coffee.length; i++) {
@@ -21,6 +24,25 @@ $('.render-coffee').click(function() {
 }
 )
 
+// sorting by rate and rendering
+$('.sort-rating').click(function() {
+    coffee.sort(function(a, b) {
+        return b.userRating - a.userRating    
+    }
+)
+clearList();   
+for(var i = 0; i < coffee.length; i++) {
+    $('.coffee-list').append('<li>' + 'Name : ' + coffee[i].coffeeName + " Category : " + coffee[i].categoryC + " Rating : " + coffee[i].userRating + '</li>')
+}
+coffee.reverse()    
+}
+)
+
+
+
+
+
+// coffee.sort(function (coffeeName, categoryC, userRating) {
 
     /*
     $('.coffee-list').append('<li>' + )
